e6c7fff95feb3380a8211ad2f9ecb46223c384d5
pierreozoux <pierre@ozoux.net>
2018-11-19 20:48:26 +0100

    Adds possibility to pass a date by env variable.

--- clean.sh
+++ clean.sh
@@ -5,13 +5,14 @@ then
   echo "Please set the remote PASSPHRASE"
 fi
 
-export DATE_TO_DELETE=`date +%Y-%m-%d -d "1 month ago"`
+export DATE_ONE_MONTH_AGO=`date +%Y-%m-%d -d "1 month ago"`
+export DATE=${DATE_TO_DELETE:-$DATE_ONE_MONTH_AGO}
 
 cd /backups
 for domain in `ls .`
 do
   export BORG_REPO=${domain}
-  export ARCHIVE=`borg list | grep ${DATE_TO_DELETE} | cut -d" " -f1`
+  export ARCHIVE=`borg list | grep ${DATE} | cut -d" " -f1`
   if [ -n "$ARCHIVE" ]; then
     borg config ./${BORG_REPO}/ append_only 0
     borg delete ::${ARCHIVE}
